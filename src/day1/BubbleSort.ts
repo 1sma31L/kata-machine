2ad7aff48c874be657c92c345eb7fa4b7464e8cd
ismail <ismailismo78@gmail.com>
2024-09-09 00:05:12 +0100

    soled: bubble sort algorithm

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] - arr[j + 1] > 0) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+        console.log(arr);
+    }
 }
+const arr = [1, 5, 7, 9, 3, 10, 15, 6];
+console.log("first time", arr);
+bubble_sort(arr);
+console.log("last time : ", arr);
